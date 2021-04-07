b7a4973ee4163dabf4f4e3a54639e0e5493e5df5
harmonco <60202702+harmonco@users.noreply.github.com>
2021-04-06 18:45:21 -0700

    QRCode Creator
    Uploading repository of files for a program to create scannable QR code address.

---
+++ QRCodeAddress.QRGenerator/Generator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QRCoder;
+using System.Drawing;
+
+namespace QRCodeAddress.QRGenerator
+{
+    public class Generator
+    {
+        //method to create new instance of QRCodeGenerator and receive input from user and generate QR code as bmp
+        public void QRGenerate()
+        {
+            QRCodeGenerator generator = new QRCodeGenerator();
+            Console.WriteLine("Please Input an address: ");
+            string Address = Console.ReadLine();
+            QRCodeData qrCodeData = generator.CreateQrCode(Address, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            qrCodeImage.Save("Address.bmp");
+           
+        }
+    }
+}
---
+++ QRCodeAddress/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using QRCodeAddress.QRGenerator;
+using System.Drawing;
+
+namespace QRCodeAddress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ceate new instance of class Generator and call QRGenerate method
+            Generator makeanaddres = new Generator();
+            makeanaddres.QRGenerate();
+        }
+    }
+}
